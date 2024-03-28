ebd649d17cf35f19eec2aeed8415cdd1e289a9e9
Amirali Akhbari <akhbariamirali42@gmail.com>
2024-03-28 21:25:20 +0330

    use channel layer from views

--- ChatApp/consumers.py
+++ ChatApp/consumers.py
@@ -1,4 +1,5 @@
 from channels.generic.websocket import WebsocketConsumer
+from asgiref.sync import async_to_sync
 import json
 
 class ChatConsumer(WebsocketConsumer):
@@ -6,8 +7,20 @@ class ChatConsumer(WebsocketConsumer):
         self.accept()
         self.send('{"type":"accept","status":"accepted"}')
 
+        # data = {
+        #     "type":"receiver_function",
+        #     "message":"Hey there"
+        # }
+
         # print(self.scope.get('session').get('test'))
-        print(self.scope.get('url_route').get('kwargs').get('name'))
+        # print(self.scope.get('url_route').get('kwargs').get('name'))
+        # print(self.channel_layer.groups)
+        # print(self.channel_name)
+        ''' باید از تابع ایسینک تو سینک استفاده کنیم '''
+        async_to_sync(self.channel_layer.group_add)('chat_group',self.channel_name)
+        # async_to_sync(self.channel_layer.group_send)('chat_group',data)
+    
+
 
     def receive(self, text_data):
         # print(json.loads(text_data)["message"])
@@ -16,4 +29,7 @@ class ChatConsumer(WebsocketConsumer):
 
     def disconnect(self, code):
         print(f"the connection closed with code => {code}")
+
+    def receiver_function(self,the_data):
+        print(the_data)
 
--- ChatApp/views.py
+++ ChatApp/views.py
@@ -1,8 +1,19 @@
 from django.shortcuts import render
+from channels.layers import get_channel_layer
+from asgiref.sync import async_to_sync
 
 # Create your views here.
 def home(request):
     # request.session['test']='hi this is me'
+
+    data = {
+            "type":"receiver_function",
+            "message":"Hey there"
+        }
+    
+    channel_layer = get_channel_layer()
+    async_to_sync(channel_layer.group_send)('chat_group',data)
+
     return render(request=request,template_name='chat/home.html')
 
 def login(request):
