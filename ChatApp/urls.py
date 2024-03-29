d5eb5ff36eaadc5c7289e3e8e3fa93d9bb1457b5
Amirali Akhbari <akhbariamirali42@gmail.com>
2024-03-29 22:19:34 +0330

    add login and logout

--- ChatApp/urls.py
+++ ChatApp/urls.py
@@ -4,6 +4,7 @@ from django.urls import path
 urlpatterns = [
     path('home/',views.home,name='home'),
     path('login/',views.login,name='login'),
+    path('logout/',views.logout,name='logout'),
     path('register/',views.register,name='register'),
     path('main/',views.main,name='main'),
     path('chat/',views.chat,name='chat')
--- ChatApp/views.py
+++ ChatApp/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render ,redirect
 from channels.layers import get_channel_layer
 from asgiref.sync import async_to_sync
-from django.contrib.auth import login as signin
-from .forms import RegistrationForm
+from django.contrib.auth import login as signin 
+from django.contrib.auth import authenticate 
+from django.contrib.auth import logout as signout 
+
+from .forms import RegistrationForm , LoginForm
 # Create your views here.
 def home(request):
     # request.session['test']='hi this is me'
@@ -18,7 +21,23 @@ def home(request):
     return render(request=request,template_name='chat/home.html')
 
 def login(request):
-    return render(request=request,template_name='chat/login.html')
+    if request.method == 'GET':
+        form = LoginForm()
+        return render(request=request,template_name='chat/login.html',context={'form':form})
+    elif request.method == 'POST':
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+            user = authenticate(request,username=username,password=password)
+            if user:
+                signin(request, user)
+                return redirect('home')
+        return render(request,'chat/login.html',{'form': form,'error':'Wrong credentials'})
+    
+def logout(request):
+    signout(request)
+    return redirect('login')    
 
 def main(request):
     return render(request=request,template_name='chat/main.html')
