cd1a1c052b9c2715f37ea05ed1508acbb2483d4e
Amirali Akhbari <akhbariamirali42@gmail.com>
2024-03-29 21:16:48 +0330

    registration part completed

--- ChatApp/views.py
+++ ChatApp/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render ,redirect
 from channels.layers import get_channel_layer
 from asgiref.sync import async_to_sync
-
+from django.contrib.auth import login as signin
+from .forms import RegistrationForm
 # Create your views here.
 def home(request):
     # request.session['test']='hi this is me'
@@ -23,7 +24,20 @@ def main(request):
     return render(request=request,template_name='chat/main.html')
 
 def register(request):
-    return render(request=request,template_name='chat/register.html')
+    if request.method == 'GET':
+        form = RegistrationForm()
+        return render(request, template_name='chat/register.html', context={'form':form})
+    elif request.method=='POST':
+        form = RegistrationForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.username = user.username.lower()
+            user.save()
+            signin(request, user)
+            return redirect('home')
+        else:
+            return render(request, 'chat/register.html', {'form': form})
+
 
 def chat(request):
     return render(request=request,template_name='chat/chat_person.html')
